509446c0314e5f22b89777974fa834f5f9f75632
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-22 23:08:56 -0300

    styled-components

---
+++ my-app/src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container } from "../../styles/GlobalStyles";
+import { Titulo, Paragrafo } from "./styled";
+
+export default function Login() {
+    return (
+        <Container>
+            <Titulo isRed={false}>
+                Login
+                <small>Oie</small>
+            </Titulo>
+            <Paragrafo>Lorem ipsum</Paragrafo>
+            <button type="button">Enviar</button>
+        </Container>
+    );
+}
