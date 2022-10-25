145697008cf0c3035c1a733fd49b3fde353c59ba
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-25 12:53:03 -0300

    react-toastify

--- my-app/src/pages/Login/index.js
+++ my-app/src/pages/Login/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+// import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Titulo, Paragrafo } from './styled';
 
 export default function Login() {
+    // toast.success('Oie, sucesso!', {
+    //     toastId: 'customId',
+    // });
     return (
         <Container>
             <Titulo isRed={false}>
--- my-app/src/routes/index.js
+++ my-app/src/routes/index.js
@@ -8,7 +8,7 @@ import Login from '../pages/Login';
 export default function Routes() {
     return (
         <Switch>
-            <MyRoute exact path={'/'} component={Login} isClosed />
+            <MyRoute exact path={'/'} component={Login} />
             <MyRoute path={'*'} component={Page404} />
         </Switch>
     );
