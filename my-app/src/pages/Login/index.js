6b61e7b4d5bcb585e91da3084ba6042788a9a635
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-25 18:13:30 -0300

    axios configurado

--- my-app/src/pages/Login/index.js
+++ my-app/src/pages/Login/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useEffect } from 'react';
 // import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Titulo, Paragrafo } from './styled';
+import axios from '../../services/axios';
 
 export default function Login() {
     // toast.success('Oie, sucesso!', {
     //     toastId: 'customId',
     // });
+
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('/');
+            console.log(response);
+        }
+
+        getData();
+    }, []);
+
     return (
         <Container>
             <Titulo isRed={false}>
