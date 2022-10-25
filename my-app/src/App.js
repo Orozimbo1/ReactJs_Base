e22cbcd73a111c57bd0d847963aebdff7158a4c8
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-24 23:23:35 -0300

    history

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
+import history from './services/history';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 
 function App() {
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <Header />
             <Routes />
             <GlobalStyles />
-        </BrowserRouter>
+        </Router>
     );
 }
 
